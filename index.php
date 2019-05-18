6ae860ab853e755b007aa848803bdd528e9fd5f4
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2019-05-17 20:42:19 -0400

    Banner Ad Test and example ad

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <title>AdBlock Detection Test</title>
     <link rel="stylesheet" type="text/css" href="./css/base.css?v=1" />
+
+    <!-- Example Ad -->
+    <link rel="stylesheet" type="text/css" href="./css/examplead.css?v=1" />
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 </head>
@@ -36,19 +40,22 @@
             </div>
             <div class="row">
                 <div class="block">
-                    <h2>Test</h2>
-                    <p>Test 3</p>
+                    <h2>Banner Ad</h2>
+                    <p>Some websites detect ad blockers by checking the height of a banner ad. If it loads it will have a height over 0.</p>
                     <div class="adtest">
                         <span id="adtest3"><i class="fas fa-spinner fa-pulse"></i>&nbsp;Checking</span>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="block">
-                    <h2>Test</h2>
-                    <p>Test 4</p>
-                    <div class="adtest">
-                        <span id="adtest4"><i class="fas fa-spinner fa-pulse"></i>&nbsp;Checking</span>
+
+            <!-- Example Ad -->
+            <div class="adBanner">
+                <div style="background: darkred; color: white;" class="blockForAd">
+                    <h2>The Tea Store</h2>
+                    <p>Hi, I'm an ad. Would you like to order a cup of tea?</p>
+
+                    <div class="adexample">
+                        <span class="ad-button-green"><a class="adtext" href="https://www.youtube.com/watch?v=6n3pFFPSlW4"><i class="fas fa-shopping-cart"></i>&nbsp;Order Now $9999.99</a></span>
                     </div>
                 </div>
             </div>
@@ -57,6 +64,9 @@
     <footer>
 
     </footer>
+
+    <!-- Banner Ad Tester -->
+    <div id="wrapfabtest"><div class="adBanner" id="adTester"></div></div>
 
     <script src="./js/ads.js" type="text/javascript"></script>
     <script type="text/javascript">var adblock = true;</script>
--- js/base.js
+++ js/base.js
@@ -26,24 +26,34 @@ function removeClass(el, className)
 
 function testAdBlock()
 {
-    var adel1 = document.getElementById('adtest1');
+    var detected = "<i class='fas fa-times-circle'></i>&nbsp;Adblock Detected";
+    var notdetected = "<i class='fas fa-check'></i>&nbsp;No Adblock";
 
+    var adel1 = document.getElementById('adtest1');
     if (document.getElementById('0Wna5W3mpo')) {
-        adel1.innerHTML = "<i class='fas fa-check'></i>&nbsp;Unblocked";
+        adel1.innerHTML = notdetected;
         addClass(adel1, "button-green");
     } else {
-        adel1.innerHTML = "<i class='fas fa-times-circle'></i>&nbsp;Blocked";
+        adel1.innerHTML = detected;
         addClass(adel1, "button-red");
     }
 
     var adel2 = document.getElementById('adtest2');
-
     if (!adblock) {
-        adel2.innerHTML = "<i class='fas fa-check'></i>&nbsp;Unblocked";
+        adel2.innerHTML = notdetected;
         addClass(adel2, "button-green");
     } else {
-        adel2.innerHTML = "<i class='fas fa-times-circle'></i>&nbsp;Blocked";
+        adel2.innerHTML = detected;
         addClass(adel2, "button-red");
+    }
+
+    var adel3 = document.getElementById('adtest3');
+    if (document.getElementById("wrapfabtest").clientHeight > 0) {
+        adel3.innerHTML = notdetected;
+        addClass(adel3, "button-green");
+    } else {
+        adel3.innerHTML = detected;
+        addClass(adel3, "button-red");
     }
 }
 
